d6ff6e893632e1f6335763527b630671fc2f4799
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-28 20:48:01 +0530

    data feed: character

--- character.js
+++ character.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 import saveFile from './helper/saveFile.js';
 
-const url = 'https://wiki.hoyolab.com/pc/genshin/entry/5'
+const url = 'https://wiki.hoyolab.com/pc/genshin/entry/1'
 
 const scraper = async () => {
   const browser = await puppeteer.launch();
