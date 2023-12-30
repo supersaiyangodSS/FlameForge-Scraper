f2f93e06709c952652599200d4f70533714e1ad5
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-30 21:59:38 +0530

    data feed: weapons/claymore

--- weapon.js
+++ weapon.js
@@ -2,8 +2,8 @@ import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 import saveFile from './helper/saveFile.js';
 
-const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n11301/?lang=EN';
-const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/1938';
+const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n12411/?lang=EN';
+const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/2043';
 
 
 const weaponExtraScraper = async () => {
