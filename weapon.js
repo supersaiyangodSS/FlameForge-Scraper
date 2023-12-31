dde2715affe2dff4c784bb464a89a5eec178094a
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-31 22:49:08 +0530

    data feed: weapon/bows

--- weapon.js
+++ weapon.js
@@ -2,8 +2,8 @@ import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 import saveFile from './helper/saveFile.js';
 
-const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n15503/?lang=EN';
-const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/2008';
+const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n15507/?lang=EN';
+const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/1936';
 
 
 const weaponExtraScraper = async () => {
