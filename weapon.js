414ecf31f6554d45291a9efc17ede1859c573047
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-01 17:35:27 +0530

    data feed: weapons/polearm

--- weapon.js
+++ weapon.js
@@ -2,8 +2,8 @@ import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 import saveFile from './helper/saveFile.js';
 
-const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n13201/?lang=EN';
-const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/2036';
+const weaponUrl = 'https://genshin.honeyhunterworld.com/i_n13101/?lang=EN';
+const extraUrl = 'https://wiki.hoyolab.com/pc/genshin/entry/1976';
 
 
 const weaponExtraScraper = async () => {
