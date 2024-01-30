e47b52fcd8c6dfb0485fa71c0823e5b12abb1942
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-30 20:40:20 +0530

    add character cheveruse

--- character.js
+++ character.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 import saveFile from './helper/saveFile.js';
 
-const url = 'https://wiki.hoyolab.com/pc/genshin/entry/1'
+const url = 'https://wiki.hoyolab.com/pc/genshin/entry/4774'
 
 const scraper = async () => {
   const browser = await puppeteer.launch();
